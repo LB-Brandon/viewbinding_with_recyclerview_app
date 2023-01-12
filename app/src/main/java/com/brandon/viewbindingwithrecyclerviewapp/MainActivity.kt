60201b9b5493927d03cc229defd6c127638096e8
LB-Brandon <rmsdud623@gmail.com>
2023-01-12 16:48:16 +0900

    Feat: initial settings
    1. set viewBinding
    2. add recyclerView

--- app/src/main/java/com/brandon/viewbindingwithrecyclerviewapp/MainActivity.kt
+++ app/src/main/java/com/brandon/viewbindingwithrecyclerviewapp/MainActivity.kt
@@ -3,9 +3,14 @@ package com.brandon.viewbindingwithrecyclerviewapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// RV를 위해서
+// 리사이클 뷰, 아이템 레이아웃, 뷰 홀더, 어댑터 가 필요하다
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
